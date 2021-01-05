e8d2fd823ab4ad9ddd658bf65535bfe63cfceb55
szczepankozdeba <48669369+szczepankozdeba@users.noreply.github.com>
2021-01-05 19:11:24 +0100

    buttoms interrupt, lcd

--- Src/main.c
+++ Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "i2c-lcd.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -40,6 +40,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 SPI_HandleTypeDef hspi2;
 
 /* USER CODE BEGIN PV */
@@ -57,7 +59,7 @@ void spi_sendrecv(uint8_t adress, uint8_t data)
 void max_init(void)
 {
 	spi_sendrecv(0x09, 0x00);         //  no decoding
-	spi_sendrecv(0x0a, 0x03);         //  brightness intensity
+	spi_sendrecv(0x0a, 0x01);         //  brightness intensity
 	spi_sendrecv(0x0b, 0x07);         //  scan limit = 8 LEDs
 	spi_sendrecv(0x0c, 0x01);         //  power down =0，normal mode = 1
 	spi_sendrecv(0x0f, 0x00);         //  no test display
@@ -68,6 +70,7 @@ void max_init(void)
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI2_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -106,8 +109,13 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SPI2_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   max_init();
+  lcd_init();
+  lcd_clear();
+  lcd_put_cur(1,1);
+  lcd_send_string("HAL");
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -130,12 +138,14 @@ int main(void)
 	  spi_sendrecv(2, 0b10101000);
 	  spi_sendrecv(3, 0b10101010);
 	  spi_sendrecv(4, 0b10101010);
-		  //spi_sendrecv(0x0a, 0x06);         //  brightness intensity
+	   //spi_sendrecv(0x0a, 0x06);         //  brightness intensity
 	  spi_sendrecv(5, 0b10101010);
 	  spi_sendrecv(6, 0b10101010);
 	  spi_sendrecv(7, 0b10101010);
 	  spi_sendrecv(8, 0b10101010);
 	  HAL_Delay(300);
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -151,6 +161,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -178,6 +189,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x2000090E;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -235,7 +298,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7|C4_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, C3_Pin|C2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -252,14 +318,16 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : C4_Pin */
   GPIO_InitStruct.Pin = C4_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(C4_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : C3_Pin C2_Pin */
   GPIO_InitStruct.Pin = C3_Pin|C2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA10 PA11 PA12 */
@@ -275,7 +343,22 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	//if(__HAL_GPIO_EXTI_GET_FLAG(GPIO_PIN_12))
+	//{
+	    spi_sendrecv(1, 0b11111111);
+	    spi_sendrecv(2, 0b00000000);
+		spi_sendrecv(3, 0b00000000);
+		spi_sendrecv(4, 0b00000000);
+		spi_sendrecv(5, 0b00000000);
+		spi_sendrecv(6, 0b00000000);
+		spi_sendrecv(7, 0b00000000);
+		spi_sendrecv(8, 0b00000000);
+	//}
+	//HAL_Delay(1000);
 
+}
 /* USER CODE END 4 */
 
 /**
